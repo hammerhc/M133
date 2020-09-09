792d9099f419c860ca3dd0c9985adc6b4214f2de
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2020-09-09 11:29:50 +0200

    Aufgabe3 changed

--- Block3/Aufgabe3/script.js
+++ Block3/Aufgabe3/script.js
@@ -10,3 +10,18 @@ function fibonacci(n) {
         number2 = result;
     }
 }
+
+var number1 = 0;
+var number2 = 1;
+var result = 0;
+
+function fibonacciRec(n) {
+    result = number1 + number2;
+    document.writeln(result);
+    console.log(result);
+    number1 = number2;
+    number2 = result;
+    if (result + number1 < n) {
+        fibonacci(n);
+    }
+}
