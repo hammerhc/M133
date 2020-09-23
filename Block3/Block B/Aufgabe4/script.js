6dfa2f0761bc3d1ebdc426b368a61f25172f0fdf
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2020-09-23 10:44:56 +0200

    Aufgabe4 finished

--- Block3/Block B/Aufgabe4/script.js
+++ Block3/Block B/Aufgabe4/script.js
@@ -1,6 +1,40 @@
+var letters = {
+    "M": 1000,
+    "D": 500,
+    "C": 100,
+    "L": 50,
+    "X": 10,
+    "V": 5,
+    "I": 1
+};
 
+var specialCases = {
+    "DCCCC": "CM",
+    "CCCC": "CD",
+    "LXXXX": "XC",
+    "XXXX": "XL",
+    "VIIII": "IX",
+    "IIII": "IV"
+};
 
-function romanNumber(number) {
-    
+function toRomanNumerals(remainder, result) {
+    for (const letter in letters) {
+        const letterValue = letters[letter];
+        if(remainder >= letterValue) {
+            const newResult = result + letter;
+            const newRemainder = remainder - letterValue;
+            if(newRemainder === 0) {
+                let resultWithSpecialCases = newResult;
+                for (const wordToMatch in specialCases) {
+                    resultWithSpecialCases = resultWithSpecialCases
+                        .replace(wordToMatch, specialCases[wordToMatch]);
+                }
+                return resultWithSpecialCases;
+            } else {
+                return toRomanNumerals(newRemainder, newResult);
+            }
+        }
+    }
 }
-romanNumber(2);
+
+console.log(toRomanNumerals(911, ""));
