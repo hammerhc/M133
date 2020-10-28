86b6caac5d36d2426232af6e57ebc3af00ca0eb1
Simon Krieger <simon_krieger@sluz.ch>
2020-10-28 11:19:30 +0100

    Block 4 Aufgabe 1 formatting changed

--- Block4/Aufgabe1/webserver.js
+++ Block4/Aufgabe1/webserver.js
@@ -2,8 +2,6 @@ import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) { // Diese Zeile einfach mal so hinnehmen, wird später erklärt
-  const text = Deno.readTextFile(
-    "index.html",
-  );
+  const text = Deno.readTextFile("index.html",);
   text.then((content) => req.respond({ body: content }));
 }
