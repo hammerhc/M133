c0a7f52ea44383dd52091e7ce0d2241c5c68b319
Simon Krieger <simon_krieger@sluz.ch>
2020-10-28 11:17:23 +0100

    Block 4 Aufgabe 2
    Site path changed

--- Block4/Aufgabe2/webserver.js
+++ Block4/Aufgabe2/webserver.js
@@ -2,14 +2,14 @@ import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) { // Diese Zeile einfach mal so hinnehmen, wird später erklärt
-    if (req.url == "/seite1") {
-        const text = Deno.readTextFile("C:/Users/vmadmin/Documents/M133/Block4/Aufgabe2/seite1.html");
-        text.then((content) => req.respond({ body: content }));
-    } else if (req.url == "/seite2") {
-        const text = Deno.readTextFile("C:/Users/vmadmin/Documents/M133/Block4/Aufgabe2/seite2.html");
-        text.then((content) => req.respond({ body: content }));
-    } else {
-        const text = Deno.readTextFile("C:/Users/vmadmin/Documents/M133/Block4/Aufgabe2/index.html");
-        text.then((content) => req.respond({ body: content }));  
-    }
+  if (req.url == "/seite1") {
+    const text = Deno.readTextFile("seite1.html");
+    text.then((content) => req.respond({ body: content }));
+  } else if (req.url == "/seite2") {
+    const text = Deno.readTextFile("seite2.html");
+    text.then((content) => req.respond({ body: content }));
+  } else {
+    const text = Deno.readTextFile("index.html");
+    text.then((content) => req.respond({ body: content }));
+  }
 }
