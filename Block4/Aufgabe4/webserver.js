fac759e887661af762fb795abbae00e14aef539b
Simon Krieger <simon_krieger@sluz.ch>
2020-11-04 10:13:25 +0100

    Block 4 Aufgabe 4 started

---
+++ Block4/Aufgabe4/webserver.js
@@ -0,0 +1,13 @@
+import {
+  Application,
+  Router,
+  send,
+} from "https://deno.land/x/oak@v6.3.1/mod.ts";
+const app = new Application();
+const router = new Router();
+router
+  .get("/", (context) => {
+    return send(context, "index.html");
+  })
+app.use(router.routes());
+app.listen({ port: 8000 });
