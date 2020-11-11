9dc646e236545ab5aeaeed66a1db3b772f4ce8b0
Simon Krieger <simon_krieger@sluz.ch>
2020-11-11 11:22:49 +0100

    Added Block 5 Aufgabe 3

---
+++ Block5/Aufgabe3/script.js
@@ -0,0 +1,53 @@
+var article = [];
+
+function load() {
+  document.querySelector("#buttonNumber").addEventListener(
+    "click",
+    getArticle,
+  );
+
+  document.querySelector("#buttonUpdate").addEventListener(
+    "click",
+    updateArticle,
+  );
+}
+
+function getArticle() {
+  var index = document.querySelector("#inputNumber").value;
+
+  if (index > 0) {
+    fetch("https://jsonplaceholder.typicode.com/posts/" + index)
+      .then((response) => response.json())
+      .then((json) => loadArticle(json))
+      .catch((error) => console.error(error));
+  } else {
+    alert("Please enter an Article greater than 0!");
+  }
+}
+
+function loadArticle(json) {
+  article = json;
+  var articleContainer = document.querySelector("#articles");
+  var articleTextarea = document.querySelector("#textareaArticle");
+  articleContainer.innerHTML = "<h1>" + article.title + "</h1><article>" +
+    article.body + "</article>";
+  articleTextarea.value = article.body;
+}
+
+function updateArticle() {
+  if (article.length > 0) {
+    article.body = document.querySelector("#textareaArticle").value;
+    fetch("https://jsonplaceholder.typicode.com/posts/" + article.id, {
+      method: "PUT",
+      body: JSON.stringify(article),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => loadArticle(json))
+      .catch((error) => console.error(error));
+  } else {
+    alert("Please load an article!");
+  }
+}
